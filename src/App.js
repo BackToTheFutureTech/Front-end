b4692f58a34ad736310d84ff9795edb22a6cffe9
wriggyj <67104461+wriggy@users.noreply.github.com>
2020-12-17 12:50:50 +0000

    Resolved conflict with new Banner component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import Footer from "./components/Footer/Footer"
-import './App.css';
 import Header from "./components/Header/Header"
 import ChooseAnOpportunity from "./components/ChooseAnOpportunity/ChooseAnOpportunity";
+import './App.css';
+import Banner from "./components/Banner/Banner";
 
 function App() {
   return (
     <div>
       <Header />
       <main>
+        <Banner />
         <ChooseAnOpportunity/>
       </main>
       <Footer />

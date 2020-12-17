9d2a95fba703eac1080b9f519ce5cd8d394f908d
wriggyj <67104461+wriggy@users.noreply.github.com>
2020-12-17 12:36:45 +0000

    Add search box to ChooseAVolunteer component

--- src/components/ChooseAnOpportunity/ChooseAnOpportunity.js
+++ src/components/ChooseAnOpportunity/ChooseAnOpportunity.js
@@ -5,10 +5,19 @@ import "./ChooseAnOpportunity.css";
 function ChooseAnOpportunity() {
     return (
         <div>
-            <div class="container">
-                <h2 class="title choose-an-opportunity__title">
+            <div className="container">
+                <h2 className="title choose-an-opportunity__title">
                     Where we need you
                 </h2>
+
+
+                <form class="form-inline my-4">
+                    <input
+                        className="form-control"
+                        type="text"
+                        placeholder="Search by Location"
+                    />
+                </form>
 
                 <div className="row">
                     <div className="col-sm">

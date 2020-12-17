39a9d6b64188fb3010095ce626f0c072e021c75b
wriggyj <67104461+wriggy@users.noreply.github.com>
2020-12-17 13:37:19 +0000

    Home page footer component - stats and testimonials

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Footer from "./components/Footer/Footer"
 import Header from "./components/Header/Header"
 import ChooseAnOpportunity from "./components/ChooseAnOpportunity/ChooseAnOpportunity";
+import HomeFooter from "./components/HomeFooter/HomeFooter";
 import './App.css';
 import Banner from "./components/Banner/Banner";
 
@@ -11,6 +12,7 @@ function App() {
       <main>
         <Banner />
         <ChooseAnOpportunity/>
+        <HomeFooter/>
       </main>
       <Footer />
     </div>

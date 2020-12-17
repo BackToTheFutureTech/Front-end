1eea8c0531ae3103f09cae52e2cfaf2d0db17cf3
wriggyj <67104461+wriggy@users.noreply.github.com>
2020-12-17 16:51:00 +0000

    Fix Opportunity Details component (class->className)

--- src/components/OpportunityDetails/OpportunityDetails.js
+++ src/components/OpportunityDetails/OpportunityDetails.js
@@ -3,17 +3,17 @@ import WrapPresents from './../../Assets/WrapPresents.jpg'
 
 function OpportunityDetails() {
     return (
-        <div class="container details">
+        <div className="container details">
 
-            <div class="row">
-                <div class="col-4">
+            <div className="row">
+                <div className="col-4">
                     <img src={WrapPresents} width="100%" alt="" />
                 </div>
-                <div class="col-5">
+                <div className="col-5">
                     <h6>Wrap Presents </h6>
                     <h5>NSPCC's Childline Service</h5>
-                    <h6>Volunteers Needed: <span class="badge badge-danger">20</span></h6>
-                    <h6>Volunteers so far: <span class="badge badge-primary">5</span></h6>
+                    <h6>Volunteers Needed: <span className="badge badge-danger">20</span></h6>
+                    <h6>Volunteers so far: <span className="badge badge-primary">5</span></h6>
                     <hr width="80%" align="left" />
                     <p>Description</p>
                     <p>• Morbi dapibus nibh ac quam efficitur pretium.<br />
@@ -27,18 +27,18 @@ function OpportunityDetails() {
                     AB48 3TH
                 </p>
                 </div>
-                <div class="col-3">
-                    <div class="apply">
-                        <p class="text-center">Date: 24/12/2020</p>
-                        <div class="container">
-                            <div class="row">
-                                <div class="col text-center">
-                                    <button type="button" class="btn btn-primary btn-sm">Apply</button>
+                <div className="col-3">
+                    <div className="apply">
+                        <p className="text-center">Date: 24/12/2020</p>
+                        <div className="container">
+                            <div className="row">
+                                <div className="col text-center">
+                                    <button type="button" className="btn btn-primary btn-sm">Apply</button>
                                 </div>
                             </div>
-                            <div class="row">
-                                <div class="col text-center">
-                                    <button type="button" class="btn btn-primary btn-sm">Apply Team</button>
+                            <div className="row">
+                                <div className="col text-center">
+                                    <button type="button" className="btn btn-primary btn-sm">Apply Team</button>
                                 </div>
                             </div>
                         </div>
